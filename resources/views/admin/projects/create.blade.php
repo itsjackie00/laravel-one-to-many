f0f75ede7c25ddbea611eb179e126d285d5b94dc
itsjackie00 <jackiedanso33@gmail.com>
2024-06-12 12:09:47 +0200

    fix

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -32,22 +32,5 @@
     </form>
 </div>
 </div>
-
-<!-- resources/views/admin/types/create.blade.php -->
-
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h1>Crea Nuova Tipologia</h1>
-        <form action="{{ route('admin.types.store') }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="name">Nome</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
-            </div>
-            <button type="submit" class="btn btn-primary">Salva</button>
-        </form>
-    </div>
 @endsection
 
